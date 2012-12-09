bffc86f1e54aef4183ad80c05efdabfb3cc16be6
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2012-12-10 01:31:43 +0200

    Bringing in namespaces, and validating against MARC21 slim, which is not much.

--- validateMarcxml.pl
+++ validateMarcxml.pl
@@ -8,10 +8,31 @@ my $document    = $ARGV[0];
 
 my $schema = XML::LibXML::Schema->new(location => $schema_file);
 
-my $parser = XML::LibXML->new;
-my $doc    = $parser->parse_file($document);
+my $parser = XML::LibXML->new (recover => 1);
+my $collectionDom = $parser->parse_file($document);
 
-eval { $schema->validate($doc) };
-die $@ if $@;
+my $xc = XML::LibXML::XPathContext->new($collectionDom);
+$xc->registerNs("marc", "http://www.loc.gov/MARC21/slim");
+my $records = $xc->findnodes('/marc:collection/marc:record');
 
-print "$document validated successfully\n";
+eval {
+	$schema->validate($collectionDom);
+	print $@;
+};
+
+
+#print($records->size . "\n");
+#my $oneRecord = $records->get_node(10);
+# print $oneRecord;
+#eval {
+#	$schema->validate($oneRecord);
+#};
+#print $@;
+
+foreach my $record ($records->get_nodelist) {
+	print("++ validating record \n");
+	eval {
+		$schema->validate($record);
+	print $@;
+	};
+}
